1d6d9dd0cbeeddaccb99885950d37935077cfb68
madhusudan405 <42300781+madhusudan405@users.noreply.github.com>
2018-08-12 07:31:34 +0530

    login defect updated

--- GitDemo/src/demo/git/Githello.java
+++ GitDemo/src/demo/git/Githello.java
@@ -5,6 +5,8 @@ public class Githello {
 	public static void main(String[] args) {
 		System.out.println("Hello Github");
 		System.out.println("i am continuing with testing of Git");
+		
+		//some of the feature updated by m team mate
 
 	}
 
