c1e05b0268b6b8750b6ccfb5f47421cdf099e3e2
madhusudan405 <madhusudan405@gmail.com>
2018-08-29 15:18:31 +0530

    TestNg
    Till @DataProvider by 2D Array is done

--- LearnAutomation/src/learn/advanced/JavaScriptExecutor.java
+++ LearnAutomation/src/learn/advanced/JavaScriptExecutor.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import useful_library.Mycode_for_ScreenShot;
+
 public class JavaScriptExecutor {
 
 	public static void main(String[] args) {
@@ -13,12 +15,16 @@ public class JavaScriptExecutor {
 		driver.get("http://www.javascriptkit.com/javatutors/event2.shtml");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		Mycode_for_ScreenShot.getScreenShot(driver, "jsexecutor_login");
 		
 		
 		//driver.findElement(By.xpath("//*[@id='contentcolumn']/div[1]/table/tbody/tr/td[2]/form/input")).click();
 		
 		//((JavascriptExecutor)driver).executeScript("inform()");
 		((JavascriptExecutor) driver).executeScript("document.getElementsByName('fd0')[0].click()");
+		//Mycode_for_ScreenShot.getScreenShot(driver, "JsExecutor_output");
+		
+		//driver.close();
 
 	}
 
